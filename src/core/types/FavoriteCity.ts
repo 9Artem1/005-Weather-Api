c8993ba072598d91683637a38af762f8109055e4
Artem <artemshapovalov1991@yandex.ru>
2023-06-01 16:11:26 +0300

    Updates

--- src/core/types/FavoriteCity.ts
+++
@@ -1,9 +0,0 @@
-export interface FavoriteCity {
-    id: string;
-    city: string;
-    coordLat: string;
-    coordLon: string;
-    weather: string;
-    wind: string;
-    temperature: number;
-  }
